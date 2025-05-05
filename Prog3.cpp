3be84088f39f0ee7d8f094247ed0baa31bca0f88
PLayL04 <88071197+PLayL04@users.noreply.github.com>
2025-05-05 12:26:01 +0700

    Prog3 and more tasks

---
+++ Prog3.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <pthread.h>
+#include <semaphore.h>
+
+const double a = -3.0;
+const double b = 2.0;
+const int n = 1000000;
+const int num_threads = 8;
+
+double integral_result = 0.0;
+sem_t semaphore;
+
+double f(double x) {
+    return 3 * x * x + 1;
+}
+
+struct ThreadData {
+    int thread_id;
+    double local_a;
+    double local_b;
+    int local_n;
+    double local_result;
+};
+
+void* compute_integral(void* arg) {
+    ThreadData* data = (ThreadData*)arg;
+    double h = (data->local_b - data->local_a) / data->local_n;
+    double sum = 0.0;
+
+    for (int i = 1; i < data->local_n; i++) {
+        double x = data->local_a + i * h;
+        sum += f(x);
+    }
+
+    sum += (f(data->local_a) + f(data->local_b)) / 2.0;
+    data->local_result = sum * h;
+
+    sem_wait(&semaphore);
+    integral_result += data->local_result;
+    sem_post(&semaphore);
+
+    return nullptr;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    pthread_t threads[num_threads];
+    ThreadData thread_data[num_threads];
+    sem_init(&semaphore, 0, 1);
+
+    double segment_length = (b - a) / num_threads;
+
+    for (int i = 0; i < num_threads; i++) {
+        thread_data[i].thread_id = i;
+        thread_data[i].local_a = a + i * segment_length;
+        thread_data[i].local_b = a + (i + 1) * segment_length;
+        thread_data[i].local_n = n / num_threads;
+        thread_data[i].local_result = 0.0;
+
+        pthread_create(&threads[i], nullptr, compute_integral, &thread_data[i]);
+    }
+
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], nullptr);
+    }
+
+    sem_destroy(&semaphore);
+
+    std::cout << "Интеграл функции 3x^2 + 1 на отрезке [" << a << ", " << b << "] равен: " << integral_result << std::endl;
+
+    return 0;
+}
